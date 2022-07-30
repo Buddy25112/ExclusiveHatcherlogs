96d32fe38e32fe2f4cf894c6cb83867a748cfb72
Buddy25112 <101816877+Buddy25112@users.noreply.github.com>
2022-07-30 03:53:32 -0600

    Update ExclusiveHatcherLogs.lua

--- ExclusiveHatcherLogs.lua
+++ ExclusiveHatcherLogs.lua
@@ -17,9 +17,9 @@ Thanks for using My Hatcher! :D
 ]]
 
 if game.PlaceId == 6284583030 or game.PlaceId == 7722306047 then 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Buddy25112/PetHatchers/main/PSXPetHatcherNormal.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Buddy25112/PetHatchers/main/PSXPetHatcherNormal.lua"),true))()
 elseif game.PlaceId == 10321372166 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Buddy25112/PetHatchers/main/PSXPetHatcherHardCore.lua"))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Buddy25112/PetHatchers/main/PSXPetHatcherHardCore.lua"),true))()
 else
 	game.Players.LocalPlayer:Kick("Error while trying to load PSX Pet Hatcher.\n\nThis game is not supported.\n\nPlease join a supported game.")
 end
